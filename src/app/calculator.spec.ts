d48f2fb050e9c21541ea5344e9f346461794a592
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-23 11:53:00 -0500

    inicio proyecto test

---
+++ src/app/calculator.spec.ts
@@ -0,0 +1,44 @@
+import { Calculator } from "./calculator";
+
+describe('Test for Calculator', () => {
+  it('#multiply should return a nine', ()=> {
+    //Arrage
+    const calculator = new Calculator();
+    //Act
+    const rta = calculator.multiply(3,3);
+    //Assert
+    expect(rta).toEqual(9);
+  });
+  it('#multiply should return a four', ()=> {
+    //Arrage
+    const calculator = new Calculator();
+    //Act
+    const rta = calculator.multiply(2,2);
+    //Assert
+    expect(rta).toEqual(4);
+  });
+  it('#divide should return a nine', ()=> {
+    //Arrage
+    const calculator = new Calculator();
+    //Act
+    const rta = calculator.divide(27,3);
+    //Assert
+    expect(rta).toEqual(9);
+  });
+  it('Matchers test', ()=> {
+    //Arrage
+    let name = "jose";
+    let name2;
+    //Assert
+    expect(name).toBeDefined();
+    expect(name2).toBe(undefined);
+
+    expect(1 + 2 === 3).toBeTruthy();
+    expect(1 + 2 === 3).toBeTruthy();
+
+    expect(5).toBeLessThan(10);
+    expect(15).toBeGreaterThan(10);
+
+    expect('123456').toMatch(/123/);
+  })
+})
