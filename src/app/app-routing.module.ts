dc1d5719a74b0494d6b05cce65425dd7b9bb5739
José Luis Zúñiga <jose@tangibledesign.co>
2024-08-20 15:42:08 -0500

    Add models

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path:"",
+  //   pathMatch: 'full',
+  //   component: AppComponent
+  // },
+  {
+    path: "products",
+    component: ProductsComponent
+  },
+  {
+    path: "pico-preview",
+    component: PicoPreviewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
