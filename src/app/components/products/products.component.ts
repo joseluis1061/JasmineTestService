a44e38894c879cf016d7c6a3e0d99a5816dbeb65
José Luis Zúñiga <jose@tangibledesign.co>
2024-08-20 16:00:59 -0500

    Add page products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
+import { ValueService } from 'src/app/services/value.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
+  products: Product[] = [];
+
+  constructor(
+    private productsService: ProductsService,
+    private valueService: ValueService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+
+  getAllProducts() {
+    this.productsService.getAllSimple()
+    .subscribe(response => {
+      this.products = response;
+    });
+  }
 
 }
